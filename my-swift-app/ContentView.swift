98f54ad72dcb45a8594edd0feaf6cb3ce72316db
hhh67 <hhio.67.2020@gmail.com>
2023-05-28 00:46:29 +0900

    first commit

---
+++ my-swift-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  my-swift-app
+//
+//  Created by 星野秀弥 on 2023/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ my-swift-app/my_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_swift_appApp.swift
+//  my-swift-app
+//
+//  Created by 星野秀弥 on 2023/05/28.
+//
+
+import SwiftUI
+
+@main
+struct my_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
