01d27a296fdd1c39297cd80361d7d4c09405faaa
hhh67 <hhio.67.2020@gmail.com>
2023-05-28 12:26:28 +0900

    Colorをhexで指定できるようなextensionを作成

---
+++ my-swift-app/Src/Extension/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  my-swift-app
+//
+//  Created by 星野秀弥 on 2023/05/28.
+//
+
+import SwiftUI
+
+extension Color {
+    init(_ hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+            blue: Double(hex & 0xFF) / 255,
+            opacity: alpha
+        )
+    }
+}
