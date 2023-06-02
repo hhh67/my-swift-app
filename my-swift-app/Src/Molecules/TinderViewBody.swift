9c34d861b766137478ccb5374f3182beaeb72dc0
hhh67 <hhio.67.2020@gmail.com>
2023-06-02 23:15:21 +0900

    スワイプアニメーション作成

--- my-swift-app/Src/Molecules/TinderViewBody.swift
+++ my-swift-app/Src/Molecules/TinderViewBody.swift
@@ -11,6 +11,8 @@ struct TinderViewBody: View {
     var _frameWidth: CGFloat
     var _frameHeight: CGFloat
     
+    @State var translation: CGSize = .zero
+    
     var body: some View {
         GeometryReader(content: {geometry in
             ZStack {
@@ -18,7 +20,7 @@ struct TinderViewBody: View {
                     Image(systemName: "flame")
                         .resizable()
                         .scaledToFill()
-                        .background(.red)
+                        .background(.white)
                     
                     LinearGradient(
                         colors: [
@@ -41,25 +43,102 @@ struct TinderViewBody: View {
                 )
                 
                 VStack {
-                    CustomText(
-                        _text: "Test",
-                        _size: 44,
-                        _weight: Font.Weight.heavy
+                    HStack {
+                        CustomSwipeText(
+                            _text: "GOOD",
+                            _size: 40,
+                            _padding: 10,
+                            _weight: Font.Weight.bold,
+                            _color: Color.green,
+                            _lineWidth: 6
+                        )
+                        
+                        Spacer()
+                        
+                        CustomSwipeText(
+                            _text: "NOPE",
+                            _size: 40,
+                            _padding: 10,
+                            _weight: Font.Weight.bold,
+                            _color: Color.red,
+                            _lineWidth: 6
+                        )
+                    }
+                    .padding(.top, geometry.size.height * 0.05)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        VStack (alignment: .leading) {
+                            CustomText(
+                                _text: "Test",
+                                _size: 44,
+                                _weight: Font.Weight.heavy
+                            )
+                            CustomText(
+                                _text: "千葉県",
+                                _size: 20
+                            )
+                            CustomText(
+                                _text: "テストです",
+                                _size: 24
+                            )
+                            CustomText(
+                                _text: "私はテストデータです",
+                                _size: 24
+                            )
+                        }
+                        
+                        Spacer()
+                        
+                        Button (action: {
+                            
+                        }, label: {
+                            Image(systemName: "info.circle.fill")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width: 40, height: 40)
+                        })
+                        .padding(.trailing, geometry.size.width * 0.03)
+                    }
+                    .frame(
+                        alignment: .leading
                     )
-                    CustomText(
-                        _text: "千葉県",
-                        _size: 20
-                    )
-                    CustomText(
-                        _text: "テストです",
-                        _size: 24
-                    )
-                    CustomText(
-                        _text: "私はテストデータです",
-                        _size: 24
-                    )
+                    .padding(.bottom, geometry.size.height * 0.05)
                 }
+                .frame(
+                    width: geometry.size.width * 0.8,
+                    height: geometry.size.height
+                )
             }
+            .offset(
+                translation
+            )
+            .rotationEffect(.degrees(Double(translation.width / 300 * 20)))
+            .gesture(
+                DragGesture()
+                    .onChanged({value in
+                        translation = value.translation
+                        print("value.location: ", value.location)
+                    })
+                    .onEnded({value in
+                        if value.startLocation.x - 150 > value.location.x {
+                            // 左側にフェードアウト
+                            translation = .init(width: -800, height: 0)
+                            
+                        } else if value.startLocation.x + 150 < value.location.x {
+                            // 右側にフェードアウト
+                            translation = .init(width: 800, height: 0)
+                        } else {
+                            translation = .zero
+                        }
+                    })
+            )
+            .animation(.spring(
+                response: 0.3,
+                dampingFraction: 0.7,
+                blendDuration: 2
+            ))
             
         })
         .offset(
@@ -83,6 +162,35 @@ struct CustomText: View {
                     size: _size,
                     weight: _weight
                 )
+            )
+            .frame(
+                alignment: .leading
+            )
+    }
+}
+
+struct CustomSwipeText: View {
+    var _text: String
+    var _size: CGFloat
+    var _padding: CGFloat
+    var _weight: Font.Weight? = Font.Weight.regular
+    var _color: Color = Color.white
+    var _lineWidth: CGFloat
+    
+    var body : some View {
+        Text(_text)
+            .foregroundColor(_color)
+            .font(.system(
+                size: _size,
+                weight: _weight
+            ))
+            .padding(.all, _padding)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(
+                        _color,
+                        lineWidth: _lineWidth
+                    )
             )
     }
 }
